213a77f4bd69d403a6bab367d9f723f4fd3ed410
codepalak <sharmapalak2503@gmail.com>
2023-10-11 12:32:30 +0530

    prime file edit

---
+++ prime.js
@@ -0,0 +1,17 @@
+let n=21;
+let count=0
+for(let i=2;i<n;i++);
+{
+    if (n%i===0)
+    {
+        count+=1
+    }
+
+}
+if (count===2)
+{
+    console.log("prime no")
+}
+else{
+    console.log(" no prime")
+}
